8cdd7ad243c473ac3a4b4b43167e81479941c9e9
Jose Marcelo Rocabado Torrez <mrocabado@hotmail.com>
2014-07-26 23:54:37 -0400

    Commit Programming Assignment 2: Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,60 @@
 
 ## Write a short comment describing this function
 
+#makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
+    inv <- NULL
 
+    set <- function(matrix) {
+        
+        if (!identical(x,matrix)) {
+            message("Clear containing env as matrix has changed)")
+            x <<- matrix
+            inv <<- NULL          
+        }
+
+    }
+    
+    get <- function() {
+        x        
+    }
+    
+    setInv <- function(matrix) {
+        message("save in containing function environment (aka cache)")
+        inv <<- matrix
+    }
+    
+
+    getInv <- function() {
+        inv
+    }
+    
+    
+    list(set = set, 
+         get = get,
+         setInv = setInv,
+         getInv = getInv)
+    
+    
 }
 
 
 ## Write a short comment describing this function
-
+#This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+#If the inverse has already been calculated (and the matrix has not changed), 
+#then the cachesolve should retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInv()
+    
+    if( !is.null(inv) ) {
+        message("getting cached inverse")
+        return( inv )
+    }
+    
+    message("computing inverse")
+    inv <- solve(x$get(), ...)
+    x$setInv(inv)
+    
+    inv
+    
 }
